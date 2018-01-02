63e6d4a872d2d7015f4ba38ecee0f415da4b8b9f
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-01-02 11:20:41 -0300

    Correcting indentation

--- stories/Main.js
+++ stories/Main.js
@@ -3,55 +3,55 @@ import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
 storiesOf('React full header', module)
-    .add('with title', () => {
-        <FullHeader
-            title="TDD"
-        />;
-    })
-    .add('with title and subtitle', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-        />;
-    })
-    .add('with title, subtitle, bgColor', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-            bgColor="#3299BB"
-        />;
-    })
-    .add('with title, subtitle, bgColor and textColor', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-            bgColor="#3299BB"
-            textColor="#FF9900"
-        />;
-    })
-    .add('with title, subtitle, bgColor, textColor and font', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-            bgColor="#3299BB"
-            textColor="#FF9900"
-            font="cursive"
-        />;
-    })
-    .add('with title, subtitle and bgImg', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-            bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
-        />;
-    })
-    .add('with title, subtitle and video', () => {
-        <FullHeader
-            title="TDD"
-            subtitle="What is tested may never break."
-            bgColor="#EBE9EB"
-            textColor="#3299BB"
-            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
-        />;
-    })
+	.add('with title', () => {
+		<FullHeader
+			title="TDD"
+		/>;
+	})
+	.add('with title and subtitle', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+		/>;
+	})
+	.add('with title, subtitle, bgColor', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+			bgColor="#3299BB"
+		/>;
+	})
+	.add('with title, subtitle, bgColor and textColor', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+			bgColor="#3299BB"
+			textColor="#FF9900"
+		/>;
+	})
+	.add('with title, subtitle, bgColor, textColor and font', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+			bgColor="#3299BB"
+			textColor="#FF9900"
+			font="cursive"
+		/>;
+	})
+	.add('with title, subtitle and bgImg', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+			bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+		/>;
+	})
+	.add('with title, subtitle and video', () => {
+		<FullHeader
+			title="TDD"
+			subtitle="What is tested may never break."
+			bgColor="#EBE9EB"
+			textColor="#3299BB"
+			video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
+		/>;
+	})
 
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import Component from '../../src/Main';
+import FullHeader from '../../src/Main';
 
-describe('Component Hello', () => {
-   it('should have h1 to display the Hello', () => {
-       const wrapper = shallow(<Component name='lyef' />);
-       expect(wrapper.find('h1')).to.have.length(1);
-   });
+describe('FullHeader Hello', () => {
+	it('should have header when mount', () => {
 
-    it('should have props for name', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        expect(wrapper.props().name).to.be.defined;
-    });
-
-    it('should create a correct DOM structure', () => {
-        const wrapper = shallow(<Component name='lyef' />);
-        const componentMock = shallow(<h1>Hello lyef!</h1>);
-        expect(wrapper.html()).to.be.equal(componentMock.html());
-    });
+	});
 });
